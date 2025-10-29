51ad99d9a5bb66d0db41c25b2b1713e50113844a
SwitchTia <oblasser.tatiana@gmail.com>
2025-10-29 14:47:46 +0100

    Feat: Exercise completed

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -8,10 +8,12 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
+
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers.splice(4,1,'Patrick');
@@ -22,22 +24,41 @@ console.log(teachers);
 const lastTeacher = teachers.pop();
 console.log(lastTeacher);
 
+
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
 console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push("Vanessa");
 console.log(teachers);
 
+
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 teachers.splice(0, 0, "Sarah")
 console.log(teachers);
 
+
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex);
+
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+
+let isTeachersEmpty;
+
+if(teachers.length > 0){
+  isTeachersEmpty = false;
+}
+else{
+  isTeachersEmpty = true;
+}
+console.log(isTeachersEmpty);
+
+//operazione terniaria
+const isTeachersEmpty2 = (teachers.length > 0)? ("false") : ("true");
+console.log(isTeachersEmpty2);
